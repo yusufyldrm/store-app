6d29e7769d47e733ada453607146d5ed064c9dce
Yusuf <yusuf_1_0_0@hotmail.com>
2022-07-25 23:47:23 +0300

    Test :: test cases added

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+export const API_URL = 'https://62286b649fd6174ca82321f1.mockapi.io/case-study';
+
 const api = axios.create({
-  baseURL: 'https://62286b649fd6174ca82321f1.mockapi.io/case-study',
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   }
